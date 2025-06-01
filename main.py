85d9fba953b4ca5642996aed86f0203b47c503a9
Deeksha Jha <deekshajha1632@gmail.com>
2025-06-01 16:31:53 +0530

    Fix: replaced 'echo is on' with actual working code in main.py, trie.py and README.md

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-ECHO is on.
+from trie.trie import Trie
+
+def test_Trie():
+    trie = Trie()
+    words = ["stack", "star", "start", "stark", "stair","sat","tassle"]
+    for word in words:
+        trie.insert(word)
+
+    print(trie.search("star"))   # True
+    print(trie.search("stair"))  # True
+    print(trie.search("stamp"))  # False
+    print(trie.search("tassle"))
+
+if __name__=="__main__":
+    test_Trie()
--- trie/trie.py
+++ trie/trie.py
@@ -1,1 +1,24 @@
-ECHO is on.
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self,word):
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+        curr.end_of_word = True
+
+    def search(self,word):
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return curr.end_of_word
