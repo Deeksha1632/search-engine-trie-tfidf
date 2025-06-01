57e85ed81cb5e6837cc225d67d4cdc8c06a447e5
Deeksha Jha <deekshajha1632@gmail.com>
2025-06-01 18:21:02 +0530

    Add autocomplete function to find words with given prefix in Trie

--- trie/trie.py
+++ trie/trie.py
@@ -22,3 +22,20 @@ class Trie:
                 return False
             curr = curr.children[ch]
         return curr.end_of_word
+    
+    def autocomplete(self,prefix):
+        def DFS(trie_node,curr_pre,words):
+            for key,val in trie_node.children.items():
+                new_pre =  curr_pre+key
+                if val.end_of_word:
+                    words.append(new_pre)
+                DFS(val,new_pre,words)
+            return 
+        curr = self.root
+        for ch in prefix:
+            if not curr.children[ch]:
+                return []
+            curr = curr.children[ch]
+        ans = []
+        DFS(curr,prefix,ans)
+        return ans
